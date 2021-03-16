b9f485dfa9d7e36603693d112875e28b228eb7af
Kuntal Sarkar <kuntalsarkar010@gmail.com>
2021-03-16 09:50:57 +0530

    updated

--- index.php
+++ index.php
@@ -20,7 +20,21 @@
 
 </head>
 <body>
-<div class="pre-loader"><h2>Loading</h2></div>
+<div class="pre-loader">
+	<div id="container">
+	  <div class="item11"></div>
+	  <div class="item12"></div>
+	  <div class="item1"></div>
+	  <div class="item2"></div>
+	  <div class="item3"></div>
+	  <div class="item4"></div>
+	  <div class="item5"></div>
+	  <div class="item6"></div>
+	  <div class="item7"></div>
+	  <div class="item8"></div>
+	  <h1>LOADING</h1>
+	</div>
+</div>
 <div class="mask"></div>
 <div class="bg-svg" style="display: none">
 	<header>
