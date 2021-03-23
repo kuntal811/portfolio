5589a1ece613c6f2c833c42f8173c17ef3721ca6
Kuntal Sarkar <kuntalsarkar010@gmail.com>
2021-03-23 18:49:28 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,14 +67,152 @@ $(document).ready(function(){
   });
 
 // Particle
-tsParticles
-  .loadJSON("programming-language-particles", "http://kuntal/portfolio/portfolio1/assets/particles.json")
+
+tsParticles.load("programming-language-particles", {
+  fpsLimit: 60,
+  particles: {
+    number: {
+      value: 160,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    },
+    color: {
+      value: "#ffffff",
+      animation: {
+        enable: true,
+        speed: 1000,
+        sync: true
+      }
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0
+      },
+      polygon: {
+        nb_sides: 5
+      },
+      image: {
+        src: "https://cdn.matteobruni.it/images/particles/github.svg",
+        width: 100,
+        height: 100
+      }
+    },
+    opacity: {
+      value: 0.5,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 3,
+        opacity_min: 0.1,
+        sync: false
+      }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 20,
+        size_min: 0.1,
+        sync: false
+      }
+    },
+    line_linked: {
+      enable: true,
+      distance: 100,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 6,
+      direction: "none",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    },
+    life: {
+      duration: {
+        sync: false,
+        value: 3
+      },
+      count: 0,
+      delay: {
+        random: {
+          enable: true,
+          minimumValue: 0.5
+        },
+        value: 1
+      }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"
+      },
+      onclick: {
+        enable: true,
+        mode: "push"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 0.8
+      },
+      repulse: {
+        distance: 200
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true,
+  background: {
+    color: "",
+    image: "",
+    position: "50% 50%",
+    repeat: "no-repeat",
+    size: "cover"
+  }
+});
+
+
+
+
+/*tsParticles
+  .loadJSON("programming-language-particles", "http://kuntal/portfolio/portfolio1/assets/logo.json")
   .then((container) => {
     //console.log("callback - tsparticles config loaded");
   })
   .catch((error) => {
     console.error(error);
-  });
+  });*/
 
 const particles = tsParticles.domItem(0);
 try{
